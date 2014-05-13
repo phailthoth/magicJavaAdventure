5326b65c1af3fd6b396577fdf80ec889b0eab7a9
phailthoth <phailthoth@gmail.com>
2014-05-13 15:30:56 -0500

    WE GOT WINDOWS
    Windoes

---
+++ magicJavaAdventure/src/magicJavaAdventure/mainWindow.java
@@ -0,0 +1,44 @@
+package magicJavaAdventure;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class mainWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					mainWindow window = new mainWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public mainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 800, 600);
+		// we're doing 800x600, right?
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
---
+++ magicJavaAdventure/src/magicJavaAdventure/setBackground.java
@@ -0,0 +1,5 @@
+package magicJavaAdventure;
+
+public class setBackground {
+
+}
