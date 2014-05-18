69c243581f346f9fa434632317820f90b07fb5d0
manawan7 <hrishih7o5@gmail.com>
2014-05-17 20:20:18 -0500

    I DID IT
    NO MORE STRUGGLING I HAVE PUT DA IMAGE ON DA FRAME
    FINALLY omg

--- magicJavaAdventure/src/magicJavaAdventure/mainWindow.java
+++ magicJavaAdventure/src/magicJavaAdventure/mainWindow.java
@@ -1,32 +1,42 @@
 package magicJavaAdventure;
 import java.io.*;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class mainWindow {
+public class mainWindow
+{
 	private static JFrame frame;
-	static Toolkit t = Toolkit.getDefaultToolkit();
-	static Image img = t.getImage("magicJavaAdventure/resources/treetrail.jpg");
+	static Image img = null;
+	
+	public static void initializeImage() throws Exception
+	{
+		img = ImageIO.read(new File("resources/treetrail.jpg"));
+	}
+			
 	//static Image img = ImageIO.read("magicJavaAdventure/resources/treetrail.jpg");
 	//static ImageIcon image = new ImageIcon("/magicJavaAdventure/resources/treetrail.jpg");
-	static JLabel background = new JLabel();
 	static Panel panel = new Panel();
 	/**
 	 * Launch the application.
+	 * @throws Exception 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					initializeImage();
 					mainWindow window = new mainWindow();
 					window.frame.setVisible(true);
+					JLabel background = new JLabel(new ImageIcon(img));
 					frame.add(panel);
-					panel.add(new JLabel( new ImageIcon("path/magicJavaAdventure/resources/treetrail.jpg")));
+					panel.add(background);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
