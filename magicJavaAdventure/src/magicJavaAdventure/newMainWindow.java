24b7787e49fd81c4b6e6344a4a27f510776e7b16
phailthoth <phailthoth@gmail.com>
2014-05-22 21:32:43 -0500

    Byebye Buttons
    Hello clickable area which I’m not done yet please help I’m stuck.

--- magicJavaAdventure/src/magicJavaAdventure/newMainWindow.java
+++ magicJavaAdventure/src/magicJavaAdventure/newMainWindow.java
@@ -1,94 +1,105 @@
 package magicJavaAdventure;
-import java.io.*;
-
-import javax.imageio.ImageIO;
-import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
 
-public class mainWindow implements ActionListener
-{
-	private static JFrame frame;
-	static Image img = null;
-	static Button btn;
-	private boolean isButtonPressed = false;
-	static Panel panel = new Panel();
-	static TextField text = new TextField(40);
-	static JLabel t = new JLabel();
-	static Color lightBlue = new Color(159, 179, 219);
-	static String[] story = {"stuff", "poop", };
-	static int lineNum;
-	
-	public static void initializeImage() throws Exception
-	{
-		img = ImageIO.read(new File("resources/treetrail.jpg"));
-	}
-	
-	/**
-	 * Launch the application.
-	 * @throws Exception 
-	 */
-	public static void main(String[] args)
-	{
-		EventQueue.invokeLater(new Runnable() 
-		{
-			public void run() 
-			{
-				try 
-				{
-					initializeImage();
-					initialize();
-					mainWindow.frame.setVisible(true);
-					JLabel background = new JLabel(new ImageIcon(img));
-					frame.add(panel);
-					panel.add(background);
-					mainWindow.addButton("ENTER");
-					panel.add(t);
-				} 
-				catch (Exception e) 
-				{
-					e.printStackTrace();
-				}
-			}
-		}
-		);
-	}
+import javax.imageio.*;
+import javax.swing.*;
 
-	/**
-	 * Create the application.
-	 */
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private static void initialize() 
-	{
-		frame = new JFrame();
-		frame.setBounds(100, 100, 800, 700);
-		// we're doing 800x600
-		frame.getContentPane().setBackground(lightBlue);
+@SuppressWarnings("serial")
+public class newMainWindow extends JPanel implements MouseListener{
+	public static JFrame frame;
+	public static JPanel panel;
+	public static JLabel speaker, text;
+	public static int s, t;
+	public static Image img;
+	public static String[] story = {
+		"[ Fire crackles, burns, and eats up everything around you ]", 
+		"Mom? Dad? [ cough ] Where are you?",
+		"[ You run out the door, hoping to find your parents. However, you don't see them. ]",
+		"MOM? DAD?",
+	};
+	
+	public static String[] speak = {
+		"",
+		"Jeffery:",
+		"",
+		"Jeffery",
+	};
+	
+	public static void init() throws Exception{
+		frame = new JFrame("clickableRegion");
+		frame.setBounds(100, 100, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//makes the jframe actually terminate after pressing "X"
+		newMainWindow.frame.setVisible(true);
+		frame.add(panel);
+		setBackground();
+		panel.add(speaker);
+		panel.add(text);
+		position(frame.getContentPane());
+		setText();
+		
 	}
 	
-	public static void addButton(String title)
-	{
-		btn = new Button(title);
-		btn.addActionListener(new mainWindow());
-		panel.add(text);
-		panel.add(btn);
+	public static void setBackground() throws Exception{
+		img = ImageIO.read(new File("resources/treetrail.jpg"));
+		JLabel background = new JLabel(new ImageIcon(img));
+		panel.add(background);
 	}
-	public void addLine(int x)
-	{
-		t.setText(story[x]);
+	
+	public static void setText(){
+		s = t = s++;
+		speaker.setText(speak[s]);
+		text.setText(story[t]);
 	}
+	
+	public static void position(Container pane){
+		pane.setLayout(null);
+		Insets insets = pane.getInsets();
+		Dimension size = speaker.getPreferredSize();
+		speaker.setBounds(50 + insets.left, 500 + insets.top, size.width, size.height);
+		text.setBounds(100 + insets.left, 550 + insets.top, size.width, size.height);
+	}
+	
 	@Override
-	public void actionPerformed(ActionEvent e) 
-	{
-		addLine(lineNum);
-		if(lineNum < story.length - 1)
-		{
-			lineNum ++;
+	public void mouseClicked(MouseEvent arg0) {
+		setText();
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) throws Exception{
+		try{
+			init();
+		}
+		catch (Exception e){
+			e.printStackTrace();
 		}
 		
 	}
+	
 }
